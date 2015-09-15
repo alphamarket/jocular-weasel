981c8a65dda967ab40dc76b688ccec3ccd19ef4a
noise2 <b.g.dariush@gmail.com>
2015-09-15 10:34:23 +0430

    @ editor: Working on stablizing an editor

---
+++ modules/defaultModule/controllers/disqusController.php
@@ -0,0 +1,60 @@
+<?php
+namespace modules\defaultModule\controllers;
+    
+/**
+ * The modules\defaultModule\controllers\disqusController
+ * @by Zinux Generator <b.g.dariush@gmail.com>
+ */
+class disqusController extends \zinux\kernel\controller\baseController
+{
+    /**
+    * The modules\defaultModule\controllers\disqusController::IndexAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function IndexAction()
+    {
+    }
+
+    /**
+    * The \modules\defaultModule\controllers\disqusController::viewAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function viewAction()
+    {
+        
+    }
+
+    /**
+    * The \modules\defaultModule\controllers\disqusController::deleteAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function deleteAction()
+    {
+        
+    }
+
+    /**
+    * The \modules\defaultModule\controllers\disqusController::editAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function editAction()
+    {
+        
+    }
+
+    /**
+    * The \modules\defaultModule\controllers\disqusController::newAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    public function newAction()
+    {
+        
+    }
+
+    /**
+    * The \modules\defaultModule\controllers\disqusController::editorAction()
+    * @by Zinux Generator <b.g.dariush@gmail.com>
+    */
+    
+}
+
--- modules/defaultModule/models/user.php
+++ modules/defaultModule/models/user.php
@@ -1,6 +1,5 @@
 <?php
 namespace modules\defaultModule\models;
-    
 /**
 * The modules\defaultModule\models\user
 * @by Zinux Generator <b.g.dariush@gmail.com>
