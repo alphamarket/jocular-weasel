1aac51ace9765a91dfdca7b4431e075d1e2f0737
noise2 <b.g.dariush@gmail.com>
2015-09-15 07:12:55 +0430

    @ modules/defaultModule/views/layout/defaultLayout.phtml: basic setup

---
+++ modules/defaultModule/models/user.php
@@ -0,0 +1,13 @@
+<?php
+namespace modules\defaultModule\models;
+    
+/**
+* The modules\defaultModule\models\user
+* @by Zinux Generator <b.g.dariush@gmail.com>
+*/
+class user
+{
+    static function hasSignedIn() {
+        return false;
+    }
+}
