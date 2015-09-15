1a9dbb22cd4378f0adc843dc44a0bd72cf6932a9
noise2 <b.g.dariush@gmail.com>
2015-09-15 14:08:12 +0430

    @ modules/defaultModule/controllers/disqusController.php: general impletation done, but not tested

--- modules/defaultModule/controllers/disqusController.php
+++ modules/defaultModule/controllers/disqusController.php
@@ -7,13 +7,18 @@ namespace modules\defaultModule\controllers;
  */
 class disqusController extends \zinux\kernel\controller\baseController
 {
+    public function Initiate()
+    {
+        parent::Initiate();
+        if(!\modules\defaultModule\models\user::HasSignedin())
+            throw new \zinux\kernel\exceptions\accessDeniedException;
+    }
     /**
     * The modules\defaultModule\controllers\disqusController::IndexAction()
     * @by Zinux Generator <b.g.dariush@gmail.com>
     */
     public function IndexAction()
-    {
-    }
+    { throw new \zinux\kernel\exceptions\accessDeniedException; }
 
     /**
     * The \modules\defaultModule\controllers\disqusController::viewAction()
@@ -21,7 +26,8 @@ class disqusController extends \zinux\kernel\controller\baseController
     */
     public function viewAction()
     {
-        
+        $this->view->disqus = \modules\defaultModule\models\disqus::first(@$this->request->indexed_param[0]);
+        \zinux\kernel\utilities\debug::_var($this->view->disqus, 1);
     }
 
     /**
@@ -30,7 +36,11 @@ class disqusController extends \zinux\kernel\controller\baseController
     */
     public function deleteAction()
     {
-        
+        \zinux\kernel\security\security::IsSecure($this->request->params, array("id"));
+        \zinux\kernel\security\security::__validate_request($this->request->params, array($this->request->params["id"]));
+        \modules\defaultModule\models\disqus::first($this->request->params["id"])->delete();
+        header("location: /");
+        exit;
     }
 
     /**
@@ -39,7 +49,16 @@ class disqusController extends \zinux\kernel\controller\baseController
     */
     public function editAction()
     {
-        
+        \zinux\kernel\security\security::IsSecure($this->request->params, array("id"));
+        \zinux\kernel\security\security::__validate_request($this->request->params, array($this->request->params["id"]));
+        $this->view->disqus = \modules\defaultModule\models\disqus::first($this->request->params["id"]);
+        if(!$this->request->IsPOST()) return;
+        \zinux\kernel\security\security::IsSecure($this->request->params, array("title", "content"));
+        $this->view->disqus->title = trim($this->request->params["title"]);
+        $this->view->disqus->context = trim($this->request->params["content"]);
+        $this->view->disqus->save();
+        header("location: /disqus/view/{$this->view->disqus->disqusid}");
+        exit;
     }
 
     /**
@@ -49,7 +68,15 @@ class disqusController extends \zinux\kernel\controller\baseController
     public function newAction()
     {
         if(!$this->request->IsPOST()) return;
-        \zinux\kernel\utilities\debug::_var($this->request->params, 1);
+        \zinux\kernel\security\security::__validate_request($this->request->params);
+        \zinux\kernel\security\security::IsSecure($this->request->params, array("title", "content"));
+        $disqus = new \modules\defaultModule\models\disqus;
+        $disqus->title = trim($this->request->params["title"]);
+        $disqus->context = trim($this->request->params["content"]);
+        $disqus->created_by =\modules\defaultModule\models\user::GetInstance()->userid;
+        $disqus->save();
+        header("location: /disqus/view/{$disqus->disqusid}");
+        exit;
     }
 }
 
---
+++ modules/defaultModule/models/disqus.php
@@ -0,0 +1,9 @@
+<?php
+namespace modules\defaultModule\models;
+/**
+* The modules\defaultModule\models\disqus
+* @by Zinux Generator <b.g.dariush@gmail.com>
+*/
+class disqus extends \ActiveRecord\Model
+{
+}
